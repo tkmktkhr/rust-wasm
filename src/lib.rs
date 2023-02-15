4e5f02c6741c576d36cded0812c114795e310ff0
tkmktkhr <mukku.wordpress@gmail.com>
2023-02-15 14:59:51 +0900

    fix previous comment

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 extern "C" {
-    pub fn alert(s: &str); // JavaScript alert function.
+    pub fn alert(s: &str); // JavaScript alert function is called.
 }
 
 #[wasm_bindgen]
