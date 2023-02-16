7aa2db1426887b814c232228809cb44e84bdbcb0
tkmktkhr <mukku.wordpress@gmail.com>
2023-02-17 07:51:17 +0900

    sample dereference

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 fn main() {
-  let mut x: i32 = 10;
-  let ref_x: &mut i32 = &mut x;
-  *ref_x = 20; // dereference
-  println!("x: {x}")
+    // dereference
+    let mut x: i32 = 10;
+    let ref_x: &mut i32 = &mut x;
+    *ref_x = 20;
+    println!("x: {x}");
+
+    let a: [i32; 6] = [10, 20, 30, 40, 50, 60];
+
+    let slice: &[i32] = &a[2..4];
+    println!("s: {slice:?}")
 }
