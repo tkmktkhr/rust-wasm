22d7436057b4d7fbf2e7d37208abaf3b1136dd88
tkmktkhr <mukku.wordpress@gmail.com>
2023-02-17 11:43:37 +0900

    sample implicit conversions from

--- src/main.rs
+++ src/main.rs
@@ -8,5 +8,17 @@ fn main() {
     let a: [i32; 6] = [10, 20, 30, 40, 50, 60];
 
     let slice: &[i32] = &a[2..4];
-    println!("s: {slice:?}")
+    println!("s: {slice:?}");
+    sample();
+}
+
+fn sample() {
+    let x: i8 = 15;
+    let y: i16 = 1000;
+
+    println!("{x} * {y} = {}", multiply(i16::from(x), y));
+}
+
+fn multiply(x: i16, y: i16) -> i16 {
+    x * y
 }
