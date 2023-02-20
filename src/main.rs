624369819bdd5e5fa2412e130ba0bcf736fd705b
tkmktkhr <mukku.wordpress@gmail.com>
2023-02-21 07:51:48 +0900

    add sample matrix array

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,25 @@ fn main() {
 }
 
 fn sample() {
+  let matrix = [
+        [101, 102, 103], // <-- the comment makes rustfmt add a newline
+        [201, 202, 203],
+        [301, 302, 303],
+    ];
+
+    println!("matrix:");
+    pretty_print(&matrix);
+
+    let transposed = transpose(matrix);
+    println!("transposed:");
+    pretty_print(&transposed);
+}
+
+fn transpose(matrix: [[i32; 3]; 3]) -> [[i32; 3]; 3] {
+  unimplemented!()
+}
+
+fn pretty_print(matrix: &[[i32; 3]; 3]) {
     let array = [10, 20, 30];
     print!("Iterating over array:");
     for n in array {
