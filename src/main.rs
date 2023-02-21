41b24165e9a93ad951d5d146855d2c93fb764c29
tkmktkhr <mukku.wordpress@gmail.com>
2023-02-22 07:50:00 +0900

    add sample matrix array transpose

--- src/main.rs
+++ src/main.rs
@@ -22,26 +22,35 @@ fn sample() {
     println!("matrix:");
     pretty_print(&matrix);
 
-    let transposed = transpose(matrix);
+    let transposed = transpose(&matrix);
     println!("transposed:");
     pretty_print(&transposed);
 }
 
-fn transpose(matrix: [[i32; 3]; 3]) -> [[i32; 3]; 3] {
-  unimplemented!()
+fn transpose(matrix: &[[i32; 3]; 3]) -> [[i32; 3]; 3] {
+  let mut result = [[0; 3]; 3];
+  for i in 0..result[0].len() {
+    for j in 0..result[0].len() {
+      result[j][i] = matrix[i][j];
+    }
+  }
+  return result
 }
 
 fn pretty_print(matrix: &[[i32; 3]; 3]) {
-    let array = [10, 20, 30];
-    print!("Iterating over array:");
-    for n in array {
-        print!(" {n}");
+    for row in matrix {
+      println!("{row:?}")
     }
-    println!();
+    // let array = [10, 20, 30];
+    // print!("Iterating over array:");
+    // for n in array {
+    //     print!(" {n}");
+    // }
+    // println!();
 
-    print!("Iterating over range:");
-    for i in 0..array.len() {
-        print!(" {}", array[i])
-    }
-    println!();
+    // print!("Iterating over range:");
+    // for i in 0..array.len() {
+    //     print!(" {}", array[i])
+    // }
+    // println!();
 }
