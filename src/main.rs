38d1a9b5db5e0a6c3dda649bef322c9303add56f
tkmktkhr <mukku.wordpress@gmail.com>
2023-02-23 11:00:55 +0900

    add transpose fn unit test

--- src/main.rs
+++ src/main.rs
@@ -54,3 +54,23 @@ fn pretty_print(matrix: &[[i32; 3]; 3]) {
     // }
     // println!();
 }
+
+#[test]
+fn test_transpose() {
+  let matrix = [
+    [101, 102, 103], //
+    [201, 202, 203],
+    [301, 302, 303],
+  ];
+  let transposed = transpose(&matrix);
+
+  assert_eq!(
+    transposed,
+    [
+      [101, 201, 301], //
+      [102, 202, 302],
+      [103, 203, 303],
+    ]
+  )
+
+}
