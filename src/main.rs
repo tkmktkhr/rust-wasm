1666dd842485dfdff2e449c96605672217ce4727
tkmktkhr <mukku.wordpress@gmail.com>
2023-02-24 08:22:31 +0900

    add advanced matrix sample

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::convert::AsRef;
+use std::fmt::Debug;
+
 fn main() {
     // dereference
     let mut x: i32 = 10;
@@ -13,7 +16,8 @@ fn main() {
 }
 
 fn sample() {
-  let matrix = [
+    // matrix practice
+    let matrix = [
         [101, 102, 103], // <-- the comment makes rustfmt add a newline
         [201, 202, 203],
         [301, 302, 303],
@@ -25,52 +29,51 @@ fn sample() {
     let transposed = transpose(&matrix);
     println!("transposed:");
     pretty_print(&transposed);
+
+    // advanced matrix practice
+    pretty_print(&[&[1, 2, 3], &[4, 5, 6], &[7, 8, 9]]);
+    pretty_print([["a", "b"], ["c", "d"]]);
+    pretty_print(vec![vec![1, 2], vec![3, 4]])
 }
 
 fn transpose(matrix: &[[i32; 3]; 3]) -> [[i32; 3]; 3] {
-  let mut result = [[0; 3]; 3];
-  for i in 0..result[0].len() {
-    for j in 0..result[0].len() {
-      result[j][i] = matrix[i][j];
+    let mut result = [[0; 3]; 3];
+    for i in 0..result[0].len() {
+        for j in 0..result[0].len() {
+            result[j][i] = matrix[i][j];
+        }
     }
-  }
-  return result
+    return result;
 }
 
-fn pretty_print(matrix: &[[i32; 3]; 3]) {
-    for row in matrix {
-      println!("{row:?}")
+fn pretty_print<T, Line, Matrix>(matrix: Matrix)
+where
+    T: Debug,
+    // A line references a slice of items
+    Line: AsRef<[T]>,
+    // A matrix references a slice of lines
+    Matrix: AsRef<[Line]>,
+{
+    for row in matrix.as_ref() {
+        println!("{:?}", row.as_ref())
     }
-    // let array = [10, 20, 30];
-    // print!("Iterating over array:");
-    // for n in array {
-    //     print!(" {n}");
-    // }
-    // println!();
-
-    // print!("Iterating over range:");
-    // for i in 0..array.len() {
-    //     print!(" {}", array[i])
-    // }
-    // println!();
 }
 
 #[test]
 fn test_transpose() {
-  let matrix = [
-    [101, 102, 103], //
-    [201, 202, 203],
-    [301, 302, 303],
-  ];
-  let transposed = transpose(&matrix);
+    let matrix = [
+        [101, 102, 103], //
+        [201, 202, 203],
+        [301, 302, 303],
+    ];
+    let transposed = transpose(&matrix);
 
-  assert_eq!(
-    transposed,
-    [
-      [101, 201, 301], //
-      [102, 202, 302],
-      [103, 203, 303],
-    ]
-  )
-
+    assert_eq!(
+        transposed,
+        [
+            [101, 201, 301], //
+            [102, 202, 302],
+            [103, 203, 303],
+        ]
+    )
 }
