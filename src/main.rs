699a9005fd4e29af6cd1437abecbdf3f3084602b
tkmktkhr <mukku.wordpress@gmail.com>
2023-02-26 14:50:02 +0900

    other sample type inference

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,14 @@ use std::fmt::Debug;
 
 fn main() {
     // dereference
-    let mut x: i32 = 10;
+    let mut x: _ = 10;
     let ref_x: &mut i32 = &mut x;
     *ref_x = 20;
     println!("x: {x}");
 
     let a: [i32; 6] = [10, 20, 30, 40, 50, 60];
 
-    let slice: &[i32] = &a[2..];
+    let slice: &[_] = &a[2..];
     println!("s: {slice:?}"); // :? is for debug.
     sample();
     type_inference();
