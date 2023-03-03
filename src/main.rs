ec3280e38ff18988b11511c7a9fabfa8c2e41bf3
tkmktkhr <mukku.wordpress@gmail.com>
2023-03-04 08:16:57 +0900

    smaple stack memory

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,7 @@ fn main() {
     static_constant();
     banner();
     shadowing();
+    stack_memory()
 }
 
 // Array, Vec, Slice
@@ -137,4 +138,17 @@ fn shadowing() {
     }
 
     println!("after: {a}");
+}
+
+fn stack_memory() {
+    let mut s1 = String::from("Hello");
+    s1.push(' ');
+    s1.push_str("world");
+    // DON'T DO THIS AT HOME! For educational purposes only.
+    // String provides no guarantees about its layout, so this could lead to
+    // undefined behavior.
+    unsafe {
+        let (capacity, ptr, len): (usize, usize, usize) = std::mem::transmute(s1);
+        println!("ptr = {ptr:#x}, len = {len}, capacity = {capacity}");
+    }
 }
