a9d170bd59cb534d7b4b12e789a2032c08c79cd9
tkmktkhr <mukku.wordpress@gmail.com>
2023-03-18 18:28:52 +0900

    move comment for self type

--- src/race.rs
+++ src/race.rs
@@ -4,6 +4,13 @@ struct Race {
   laps: Vec<i32>,
 }
 
+// method receiver.
+// &self: borrows the object from the caller using a shared and immutable reference. The object can be used again afterwards.
+// &mut self: borrows the object from the caller using a unique and mutable reference. The object can be used again afterwards.
+// self: takes ownership of the object and moves it away from the caller. The method becomes the owner of the object. The object will be dropped (deallocated) when the method returns, unless its ownership is explicitly transmitted.
+// mut self: same as above, but while the method owns the object, it can mutate it too. Complete ownership does not automatically mean mutability.
+// No receiver: this becomes a static method on the struct. Typically used to create constructors which are called new by convention.
+
 impl Race {
   fn new(name: &str) -> Race {
     // No receiver, a static method
@@ -41,5 +48,5 @@ pub fn fn_race() {
   race.add_lap(35);
   race.print_laps();
   race.finish()
+  // race.add_lap(35)
 }
-
