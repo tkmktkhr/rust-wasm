014aa325311d0d69ed1d547114c3aa1179407739
tkmktkhr <mukku.wordpress@gmail.com>
2023-03-22 07:47:03 +0900

    sample destructure tuple

--- src/main.rs
+++ src/main.rs
@@ -39,7 +39,8 @@ fn main() {
   fn_race();
   pattern_matching();
   destructure_enum();
-  destructure_struct()
+  destructure_struct();
+  destructure_tuple()
 }
 
 // Array, Vec, Slice
@@ -495,4 +496,15 @@ fn destructure_struct() {
     }
   };
   println!("result is {res:?}")
+}
+
+fn destructure_tuple() {
+  let triple = [0, -2, -3];
+  println!("Tell me about {triple:?}");
+
+  match triple {
+    [0, y, z] => println!("First is 0, y = {y}, and z = {z}"),
+    [1, ..] => println!("First is 1 and the rest were ignored"),
+    _ => println!("All elements were ignored"),
+  }
 }
