fc51898c13de8f6695cc58b6637ffefe2019e27d
tkmktkhr <mukku.wordpress@gmail.com>
2023-03-28 07:51:12 +0900

    sample if let

--- src/main.rs
+++ src/main.rs
@@ -45,6 +45,7 @@ fn main() {
   destructure_array();
   match_guards();
   block();
+  if_let()
 }
 
 // Array, Vec, Slice
@@ -579,4 +580,40 @@ fn block() {
     z - y
   };
   println!("x: {x}");
+}
+
+fn if_let() {
+  // All have type `Option<i32>`
+  let number = Some(7);
+  // let letter: Option<i32> = None;
+  let letter: Option<i32> = Some(10);
+  let emoticon: Option<i32> = None;
+
+  // The `if let` construct reads: "if `let` destructures `number` into `Some(i)`, evaluate the block (`{}`).
+  if let Some(i) = number {
+    println!("Matched {:?}", i);
+  }
+
+  // If you need to specify a failure, use an else:
+  if let Some(i) = letter {
+    println!("Matched {:?}!", i);
+    // letter // try adding a return type: `-> Option<i32> `
+  } else {
+    // Destructure failed. Change to the failure case.
+    println!("Didn't match a number. Let's go with a letter!");
+    // Some(1)// try adding a return type: `-> Option<i32> `
+  }
+
+  // // Provide an altered failing condition.
+  let i_like_letters = false;
+
+  if let Some(i) = emoticon {
+    println!("Matched {:?}!", i);
+  // Destructure failed. Evaluate an `else if` condition to see if the alternate failure branch should be taken:
+  } else if i_like_letters {
+    println!("Didn't match a number. Let's go with a letter!");
+  } else {
+    // The condition evaluated false. This branch is the default:
+    println!("I don't like letters. Let's go with an emoticon :)!");
+  }
 }
