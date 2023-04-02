4e003edb8e562f85ae2f269d0c2668276de51bf8
tkmktkhr <mukku.wordpress@gmail.com>
2023-04-03 07:51:09 +0900

    add vec_sample v3

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -46,9 +46,14 @@ pub mod tools {
     v2.push(9999);
     println!("v2: len = {}, capacity = {}", v2.len(), v2.capacity());
 
-    // let mut v2 = Vec::with_capacity(v1.len() + 1);
-    // v2.extend(v1.iter());
-    // v2.push(9999);
-    // println!("v2: len = {}, capacity = {}", v2.len(), v2.capacity());
+    // Canonical macro to initialize a vector with elements.
+    let mut v3 = vec![0,0,1,2,3,4];
+
+    v3.retain(|x| x % 2 == 0); // if the content is true, pick it up.
+    println!("{v3:?}");
+
+    // Remove consecutive duplicates.
+    v3.dedup();
+    println!("{v3:?}");
   }
 }
