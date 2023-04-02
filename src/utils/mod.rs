4cd3e5cded2ac9b26dfd0b755ab001344e37d58e
tkmktkhr <mukku.wordpress@gmail.com>
2023-04-02 09:53:12 +0900

    add vec_sample v2

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -38,6 +38,17 @@ pub mod tools {
     // The data it contains is stored on the heap. This means the amount of data doesn’t need to be known at compile time. It can grow or shrink at runtime.
     let mut v1 = Vec::new();
     v1.push(42);
+    v1.push(43);
     println!("v1: len = {}, capacity = {}", v1.len(), v1.capacity());
+
+    let mut v2 = Vec::with_capacity(v1.len() + 1);
+    v2.extend(v1.iter());
+    v2.push(9999);
+    println!("v2: len = {}, capacity = {}", v2.len(), v2.capacity());
+
+    // let mut v2 = Vec::with_capacity(v1.len() + 1);
+    // v2.extend(v1.iter());
+    // v2.push(9999);
+    // println!("v2: len = {}, capacity = {}", v2.len(), v2.capacity());
   }
 }
