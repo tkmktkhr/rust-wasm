4b7cac80d39f77b93221a065a2c7463a0c196907
tkmktkhr <mukku.wordpress@gmail.com>
2023-04-14 08:10:50 +0900

    add cmt about module path type

--- src/utils/tools.rs
+++ src/utils/tools.rs
@@ -117,3 +117,15 @@ pub fn rc() {
   println!("{:p}, {:p}, {:p}", s, t, u); // same pointer
   println!("{:?}", Rc::strong_count(&s));
 }
+
+// pub fn path() {
+// Paths are resolved as follows:
+
+// 1. As a relative path:
+// foo or self::foo refers to foo in the current module,
+// super::foo refers to foo in the parent module.
+
+// 2. As an absolute path:
+// crate::foo refers to foo in the root of the current crate,
+// bar::foo refers to foo in the bar crate.
+// }
