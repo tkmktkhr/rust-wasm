b14ee061d4cbb13a7494243d525f2e8137738da7
tkmktkhr <mukku.wordpress@gmail.com>
2023-04-16 11:04:30 +0900

    add generic data type sample

--- src/utils/generics.rs
+++ src/utils/generics.rs
@@ -1,1 +1,17 @@
-// tba
+#[derive(Debug)]
+struct Point<T> {
+  x: T,
+  y: T, // warning.
+}
+
+pub fn generics_sample() {
+  let integer = Point { x: 5, y: 10 };
+  let float = Point { x: 1.0, y: 4.0 };
+  let test = Point {
+    x: "x".to_string(),
+    y: "y".to_string(),
+  };
+  let x = &test.x;
+  println!("{x:?}");
+  println!("{integer:?} and {float:?} and {test:?}");
+}
