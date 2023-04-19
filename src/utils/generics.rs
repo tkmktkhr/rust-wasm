0e19b202daeac6ed5f9aac0f873b3a73a98bede7
tkmktkhr <mukku.wordpress@gmail.com>
2023-04-20 07:49:23 +0900

    add cmt about trait sample

--- src/utils/generics.rs
+++ src/utils/generics.rs
@@ -49,3 +49,44 @@ pub fn generic_method() {
   // }
   // let float = Option_f64::Some(5.0);
 }
+
+// Rust lets you abstract over types with traits. They’re similar to interfaces:
+
+
+// pub fn trait() {
+
+// }
+
+// trait Pet {
+//   fn name(&self) -> String;
+// }
+
+// struct Dog {
+//   name: String,
+// }
+
+// struct Cat;
+
+// impl Pet for Dog {
+//   fn name(&self) -> String {
+//       self.name.clone()
+//   }
+// }
+
+// impl Pet for Cat {
+//   fn name(&self) -> String {
+//       String::from("The cat") // No name, cats won't respond to it anyway.
+//   }
+// }
+
+// fn greet<P: Pet>(pet: &P) {
+//   println!("Who's a cutie? {} is!", pet.name());
+// }
+
+// fn main() {
+//   let fido = Dog { name: "Fido".into() };
+//   greet(&fido);
+
+//   let captain_floof = Cat;
+//   greet(&captain_floof);
+// }
