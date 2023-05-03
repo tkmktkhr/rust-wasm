e836d139c090cb964643aea877dd2cd1db116d0b
tkmktkhr <mukku.wordpress@gmail.com>
2023-05-04 08:12:33 +0900

    add count_lines fn

--- src/utils/traits.rs
+++ src/utils/traits.rs
@@ -214,3 +214,20 @@ pub fn into_sample() {
   let bigger: i32 = 123i16.into();
   println!("into: {s}, {addr}, {one}, {bigger}");
 }
+
+// Read and Write
+use std::io::{BufRead, BufReader, Read, Result};
+
+fn count_lines<R: Read>(reader: R) -> usize {
+  let buf_reader = BufReader::new(reader);
+  buf_reader.lines().count()
+}
+
+// fn main() -> Result<()> {
+//     let slice: &[u8] = b"foo\nbar\nbaz\n";
+//     println!("lines in slice: {}", count_lines(slice));
+
+//     let file = std::fs::File::open(std::env::current_exe()?)?;
+//     println!("lines in file: {}", count_lines(file));
+//     Ok(())
+// }
