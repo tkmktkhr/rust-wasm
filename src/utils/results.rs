fa3ca15564637c18a82791366305ad2b39e7543b
tkmktkhr <mukku.wordpress@gmail.com>
2023-05-27 08:30:28 +0900

    add handled error sample file_read

--- src/utils/results.rs
+++ src/utils/results.rs
@@ -1,4 +1,4 @@
-use std::fs;
+// use std::fs;
 use std::io::{self, Read};
 
 
@@ -33,4 +33,48 @@ fn read_username(path: &str) -> Result<String, io::Error> {
     Ok(_) =>  Ok(username),
     Err(err) => Err(err),
   }
+}
+
+// Converting Error types
+use std::error::Error;
+use std::fmt::{self, Display, Formatter};
+use std::fs::{self, File};
+// use std::io::{self, Read};
+
+#[derive(Debug)]
+enum ReadUsernameError {
+    IoError(io::Error),
+    EmptyUsername(String),
+}
+
+impl Error for ReadUsernameError {}
+
+impl Display for ReadUsernameError {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        match self {
+            Self::IoError(e) => write!(f, "IO error: {e}"),
+            Self::EmptyUsername(filename) => write!(f, "Found no username in {filename}"),
+        }
+    }
+}
+
+impl From<io::Error> for ReadUsernameError {
+    fn from(err: io::Error) -> ReadUsernameError {
+        ReadUsernameError::IoError(err)
+    }
+}
+
+fn read_username1(path: &str) -> Result<String, ReadUsernameError> {
+    let mut username = String::with_capacity(100);
+    File::open(path)?.read_to_string(&mut username)?;
+    if username.is_empty() {
+        return Err(ReadUsernameError::EmptyUsername(String::from(path)));
+    }
+    Ok(username)
+}
+
+pub fn file_read2() {
+    //fs::write("config.dat", "").unwrap();
+    let username = read_username1("./src/utils/results.json");
+    println!("username or error: {username:?}");
 }
