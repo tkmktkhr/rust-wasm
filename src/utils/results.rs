71129c4bbb5483e2f27e9439ad0248f98a45d335
tkmktkhr <mukku.wordpress@gmail.com>
2023-05-28 09:21:38 +0900

    cmt for sample file read fn second

--- src/utils/results.rs
+++ src/utils/results.rs
@@ -1,9 +1,10 @@
-// use std::fs;
+use std::error::Error;
+use std::fmt::{self, Display, Formatter};
+use std::fs::{self, File};
 use std::io::{self, Read};
 
-
 // Propagate Errors
-// The try-operator ? is used to return errors to the caller. 
+// The try-operator ? is used to return errors to the caller.
 /*
   match some_expression {
     Ok(value) => value,
@@ -12,12 +13,12 @@ use std::io::{self, Read};
 */
 
 pub fn file_read() {
-    // create new file
-    // fs::write("results2.json", "alice").unwrap();
-    // let username = read_username("./results2.json"); // absolute path.
+  // create new file
+  // fs::write("results2.json", "alice").unwrap();
+  // let username = read_username("./results2.json"); // absolute path.
 
-    let username = read_username("./src/utils/results.json"); // absolute path.
-    println!("username or error: {username:?}");
+  let username = read_username("./src/utils/results.json"); // absolute path.
+  println!("username or error: {username:?}");
 }
 
 fn read_username(path: &str) -> Result<String, io::Error> {
@@ -30,51 +31,47 @@ fn read_username(path: &str) -> Result<String, io::Error> {
 
   let mut username = String::new();
   match username_file.read_to_string(&mut username) {
-    Ok(_) =>  Ok(username),
+    Ok(_) => Ok(username),
     Err(err) => Err(err),
   }
 }
 
 // Converting Error types
-use std::error::Error;
-use std::fmt::{self, Display, Formatter};
-use std::fs::{self, File};
-// use std::io::{self, Read};
 
-#[derive(Debug)]
-enum ReadUsernameError {
-    IoError(io::Error),
-    EmptyUsername(String),
-}
+// #[derive(Debug)]
+// enum ReadUsernameError {
+//     IoError(io::Error),
+//     EmptyUsername(String),
+// }
 
-impl Error for ReadUsernameError {}
+// impl Error for ReadUsernameError {}
 
-impl Display for ReadUsernameError {
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        match self {
-            Self::IoError(e) => write!(f, "IO error: {e}"),
-            Self::EmptyUsername(filename) => write!(f, "Found no username in {filename}"),
-        }
-    }
-}
+// impl Display for ReadUsernameError {
+//     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+//         match self {
+//             Self::IoError(e) => write!(f, "IO error: {e}"),
+//             Self::EmptyUsername(filename) => write!(f, "Found no username in {filename}"),
+//         }
+//     }
+// }
 
-impl From<io::Error> for ReadUsernameError {
-    fn from(err: io::Error) -> ReadUsernameError {
-        ReadUsernameError::IoError(err)
-    }
-}
+// impl From<io::Error> for ReadUsernameError {
+//     fn from(err: io::Error) -> ReadUsernameError {
+//         ReadUsernameError::IoError(err)
+//     }
+// }
 
-fn read_username1(path: &str) -> Result<String, ReadUsernameError> {
-    let mut username = String::with_capacity(100);
-    File::open(path)?.read_to_string(&mut username)?;
-    if username.is_empty() {
-        return Err(ReadUsernameError::EmptyUsername(String::from(path)));
-    }
-    Ok(username)
-}
+// fn read_username1(path: &str) -> Result<String, ReadUsernameError> {
+//     let mut username = String::with_capacity(100);
+//     File::open(path)?.read_to_string(&mut username)?;
+//     if username.is_empty() {
+//         return Err(ReadUsernameError::EmptyUsername(String::from(path)));
+//     }
+//     Ok(username)
+// }
 
-pub fn file_read2() {
-    //fs::write("config.dat", "").unwrap();
-    let username = read_username1("./src/utils/results.json");
-    println!("username or error: {username:?}");
-}
+// pub fn file_read2() {
+//     //fs::write("config.dat", "").unwrap();
+//     let username = read_username1("./src/utils/results.json");
+//     println!("username or error: {username:?}");
+// }
