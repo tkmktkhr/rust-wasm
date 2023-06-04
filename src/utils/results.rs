56197487cb70bc265ad0fbd519ad27c786f22cef
tkmktkhr <mukku.wordpress@gmail.com>
2023-06-04 11:32:30 +0900

    add thiserror macro impl

--- src/utils/results.rs
+++ src/utils/results.rs
@@ -1,8 +1,8 @@
-use std::error::Error;
+// use std::error::Error;
 use std::fmt::{self, Display, Formatter};
 use std::fs::{self, File};
 use std::io::{self, Read};
-// use thiserror::Error;
+use thiserror::Error;
 
 // Propagate Errors
 // The try-operator ? is used to return errors to the caller.
@@ -40,22 +40,25 @@ fn read_username1(path: &str) -> Result<String, io::Error> {
 // Converting Error types
 // and
 // Deriving Error Enums: thiserror
-#[derive(Debug)]
+#[derive(Debug, Error)]
 enum ReadUsernameError {
+  #[error("This is thiserror macro - Could not read: {0}")]
   IoError(io::Error),
+  #[error("This is thiserror macro - Found no username in {0}")]
   EmptyUsername(String),
 }
 
-impl Error for ReadUsernameError {}
+// impl Error for ReadUsernameError {} // this is unnecessary due to thiserror.
 
-impl Display for ReadUsernameError {
-  fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-    match self {
-      Self::IoError(e) => write!(f, "IO error: {e}"),
-      Self::EmptyUsername(filename) => write!(f, "Found no username in {filename}"),
-    }
-  }
-}
+// this is unnecessary due to thiserror.
+// impl Display for ReadUsernameError {
+//   fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+//     match self {
+//       Self::IoError(e) => write!(f, "IO error: {e}"),
+//       Self::EmptyUsername(filename) => write!(f, "Found no username in {filename}"),
+//     }
+//   }
+// }
 
 impl From<io::Error> for ReadUsernameError {
   fn from(err: io::Error) -> ReadUsernameError {
@@ -76,7 +79,11 @@ fn read_username2(path: &str) -> Result<String, ReadUsernameError> {
 pub fn file_read2() {
   //fs::write("config.dat", "").unwrap();
   let username = read_username2("./src/utils/results.json");
-  println!("read2: username or error: {username:?}");
+  match username {
+    Ok(username) => println!("Username: {username}"),
+    Err(err) => println!("Error: {err}"),
+  }
+  // println!("read2: username or error: {:?}", &username);
 }
 
 // `expression?` works the same as
@@ -84,5 +91,3 @@ pub fn file_read2() {
 //   Ok(value) => value,
 //   Err(err)  => return Err(From::from(err)),
 // }
-
-
