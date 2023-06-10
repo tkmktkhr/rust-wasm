73d48d1f2e1036a0e7fc8669ab9428ba9d1af277
tkmktkhr <mukku.wordpress@gmail.com>
2023-06-10 11:53:27 +0900

    add unit test sample tests

--- src/utils/tests.rs
+++ src/utils/tests.rs
@@ -9,17 +9,17 @@ pub fn first_word(text: &str) -> &str {
   &res
 }
 
-// #[test]
-// fn test_empty() {
-//   assert_eq!(first_word(""), "");
-// }
+#[test]
+fn test_empty() {
+  assert_eq!(first_word(""), "");
+}
 
-// #[test]
-// fn test_single_word() {
-//   assert_eq!(first_word("hello"), "Hello");
-// }
+#[test]
+fn test_single_word() {
+  assert_eq!(first_word("hello"), "Hello");
+}
 
-// #[test]
-// fn test_multiple_words() {
-//   assert_eq!("Hello World", "Hello");
-// }
+#[test]
+fn test_multiple_words() {
+  assert_eq!("Hello World", "Hello");
+}
