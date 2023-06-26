c03fe1e1acde4da4adb9b7239e474679ae8d7787
tkmktkhr <mukku.wordpress@gmail.com>
2023-06-27 08:30:11 +0900

    add unsafe trait example

--- src/utils/unsafes.rs
+++ src/utils/unsafes.rs
@@ -123,3 +123,17 @@ pub fn unsafe_extern() {
 // Unsafe trait
 use std::mem::size_of_val;
 use std::slice;
+
+/// ...
+/// # Safety
+/// The type must have a defined representation and no padding.
+pub unsafe trait AsBytes {
+  fn as_bytes(&self) -> &[u8] {
+      unsafe {
+          slice::from_raw_parts(self as *const Self as *const u8, size_of_val(self))
+      }
+  }
+}
+
+// Safe because u32 has a defined representation and no padding.
+unsafe impl AsBytes for u32 {}
