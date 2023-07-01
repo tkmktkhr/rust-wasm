707aa61e3518d2b90ef6be1e6ff248fa35f4b16d
tkmktkhr <mukku.wordpress@gmail.com>
2023-07-02 08:38:30 +0900

    add notes thread sample

--- src/utils/concurrencies.rs
+++ src/utils/concurrencies.rs
@@ -28,12 +28,16 @@ pub fn conc_thread() {
 //     });
 // }
 
-pub fn normal_scoped_thread() {
-  let s = String::from("Hello");
 
-  thread::scope(|scope| {
-      scope.spawn(|| {
-          println!("Length: {}", s.len());
-      });
-  });
-}
+// The reason for that is that when the thread::scope function completes, all the threads are guaranteed to be joined, so they can return borrowed data.
+// Normal Rust borrowing rules apply: you can either borrow mutably by one thread, or immutably by any number of threads.
+
+// pub fn normal_scoped_thread() {
+//   let s = String::from("Hello");
+
+//   thread::scope(|scope| {
+//       scope.spawn(|| {
+//           println!("Length: {}", s.len());
+//       });
+//   });
+// }
