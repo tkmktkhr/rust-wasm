e59bb4ffd47c5cfac3db1ff59fb5b443c0666759
tkmktkhr <mukku.wordpress@gmail.com>
2023-07-24 09:35:30 +0900

    add cmt arc mutex

--- src/utils/concurrencies.rs
+++ src/utils/concurrencies.rs
@@ -149,18 +149,22 @@ pub mod shared_state {
     println!("v: {:?}", v.lock().unwrap());
   }
 
+  // v is wrapped in both Arc and Mutex, because their concerns are orthogonal.
+  // Wrapping a Mutex in an Arc is a common pattern to share mutable state between threads.
+  // v: Arc<_> needs to be cloned as v2 before it can be moved into another thread. Note move was added to the lambda signature.
+  // Blocks are introduced to narrow the scope of the LockGuard as much as possible.
   pub fn arc_mutex() {
     let v = Arc::new(Mutex::new(vec![10, 20, 30]));
 
     let v2 = Arc::clone(&v);
     let handle = thread::spawn(move || {
-        let mut v2 = v2.lock().unwrap();
-        v2.push(10);
+      let mut v2 = v2.lock().unwrap();
+      v2.push(10);
     });
 
     {
-        let mut v = v.lock().unwrap();
-        v.push(1000);
+      let mut v = v.lock().unwrap();
+      v.push(1000);
     }
 
     handle.join().unwrap();
