1a3ae655ecb191c0072c79cf16c3f66fc8ccfd4c
tkmktkhr <mukku.wordpress@gmail.com>
2023-09-09 17:19:12 +0900

    add ploygon_struct file before implementation.

---
+++ src/utils/exercises/polygon_struct.rs
@@ -0,0 +1,33 @@
+#![allow(unused_variables, dead_code)]
+
+pub struct Point {
+    // add fields
+}
+
+impl Point {
+    // add methods
+}
+
+pub struct Polygon {
+    // add fields
+}
+
+impl Polygon {
+    // add methods
+}
+
+pub struct Circle {
+    // add fields
+}
+
+impl Circle {
+    // add methods
+}
+
+pub enum Shape {
+    Polygon(Polygon),
+    Circle(Circle),
+}
+
+#[allow(dead_code)]
+fn polygon_struct() {}
