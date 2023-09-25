ebc4f34f7bd0d86b730df81f023375e0ef76654d
tkmktkhr <mukku.wordpress@gmail.com>
2023-09-25 09:09:57 +0900

    create sync_channel for dining philosopher

--- src/utils/exercises.rs
+++ src/utils/exercises.rs
@@ -1,6 +1,6 @@
 pub mod array_for_loops;
+pub mod dining_philosophers;
 pub mod polygon_struct;
 pub mod simple_gui_library;
 pub mod storing_book;
 pub mod strings_and_iterators;
-pub mod dining_philosophers;
--- src/utils/exercises/dining_philosophers.rs
+++ src/utils/exercises/dining_philosophers.rs
@@ -5,38 +5,39 @@ use std::time::Duration;
 struct Fork;
 
 struct Philosopher {
-    name: String,
-    left_fork: Arc<Mutex<Fork>>,
-    right_fork: Arc<Mutex<Fork>>,
-    thoughts: mpsc::SyncSender<String>,
+  name: String,
+  left_fork: Arc<Mutex<Fork>>,
+  right_fork: Arc<Mutex<Fork>>,
+  thoughts: mpsc::SyncSender<String>,
 }
 
 impl Philosopher {
-    fn think(&self) {
-        self.thoughts
-            .send(format!("Eureka! {} has a new idea!", &self.name))
-            .unwrap();
-    }
+  fn think(&self) {
+    self
+      .thoughts
+      .send(format!("Eureka! {} has a new idea!", &self.name))
+      .unwrap();
+  }
 
-    fn eat(&self) {
-        // Pick up forks...
-        println!("{} is eating...", &self.name);
-        thread::sleep(Duration::from_millis(10));
-    }
+  fn eat(&self) {
+    // Pick up forks...
+    println!("{} is eating...", &self.name);
+    thread::sleep(Duration::from_millis(10));
+  }
 }
 
-static PHILOSOPHERS: &[&str] =
-    &["Socrates", "Plato", "Aristotle", "Thales", "Pythagoras"];
+static PHILOSOPHERS: &[&str] = &["Socrates", "Plato", "Aristotle", "Thales", "Pythagoras"];
 
 pub fn dining_philosophers() {
-    // Create forks
-    let forks = (0..PHILOSOPHERS.len())
-        .map(|_| Arc::new(Mutex::new(Fork)))
-        .collect::<Vec<_>>();
+  let (tx, rx) = mpsc::sync_channel::<i32>(10);
+  // Create forks
+  let forks = (0..PHILOSOPHERS.len())
+    .map(|_| Arc::new(Mutex::new(Fork)))
+    .collect::<Vec<_>>();
 
-    // Create philosophers
+  // Create philosophers
 
-    // Make each of them think and eat 100 times
+  // Make each of them think and eat 100 times
 
-    // Output their thoughts
+  // Output their thoughts
 }
